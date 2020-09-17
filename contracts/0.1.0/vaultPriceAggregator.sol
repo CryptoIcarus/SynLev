1c7080fae30433c2b2cef7e0e046e30ad570ef3a
CryptoIcarus <icarus@synlev.com>
2020-09-17 13:14:37 +0200

    Testing function added

--- contracts/0.1.0/vaultPriceAggregator.sol
+++ contracts/0.1.0/vaultPriceAggregator.sol
@@ -36,7 +36,7 @@ contract vaultPriceAggregator is Owned {
   uint256 public maxUpdates = 10;
 
 
-  function priceRequest(address vault, uint256 lastUpdated)
+  function priceRequestREAL(address vault, uint256 lastUpdated)
   public
   view
   returns(uint256[] memory, uint256)
@@ -56,6 +56,37 @@ contract vaultPriceAggregator is Owned {
       return(new uint256[](0), lastUpdated);
     }
   }
+
+  //FOR TESTING PURPOSES ONLY AS CHAINLINK HAS NOT BEEN ACTIVELY UPDATING ORACLE
+  //DATA WE GO BACK IN TIME
+  function priceRequest(address vault, uint256 lastUpdated)
+  public
+  view
+  returns(uint256[] memory, uint256)
+  {
+    uint256 currentRound;
+    if(lastUpdated == 0) {
+      currentRound = 18446744073709552369;
+    }
+    else {
+      currentRound = lastUpdated + block.timestamp % 5
+    }
+
+    if(currentRound > lastUpdated) {
+      uint256 pricearrayLength = 1 + currentRound - lastUpdated;
+      pricearrayLength = pricearrayLength > maxUpdates ? maxUpdates : pricearrayLength;
+      uint256[] memory pricearray = new uint256[] (pricearrayLength);
+      pricearray[0] =  uint256(refVault[vault].ref.getAnswer(lastUpdated));
+      for(uint i = 1; i < pricearrayLength; i++) {
+        pricearray[pricearrayLength - i] = uint256(refVault[vault].ref.getAnswer(1 + currentRound - i));
+      }
+      return(pricearray, currentRound);
+    }
+    else {
+      return(new uint256[](0), lastUpdated);
+    }
+  }
+
 
 
   function setMaxUpdates(uint256 amount) public onlyOwner() {
