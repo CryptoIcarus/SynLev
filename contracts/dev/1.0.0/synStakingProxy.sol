3c1eb0ac370f61c238202c8cd88f94927dabfe42
CryptoIcarus <icarus@shitcoin.fun>
2020-10-28 16:31:24 +0100

    Add staking proxy

---
+++ contracts/dev/1.0.0/synStakingProxy.sol
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////
+//SYNLEV FEES PROXY CONTRACT V 0.1.0
+//////////////////////////
+
+pragma solidity >= 0.6.4;
+
+import './ownable.sol';
+
+contract synStakingProxy is Owned {
+
+  address payable public feeRecipient;
+
+  receive() external payable {}
+
+  function forwardfees() public {
+    require(feeRecipient != address(0));
+    feeRecipient.transfer(address(this).balance);
+  }
+
+  function setFeeRecipient(address payable account) public onlyOwner() {
+    feeRecipient = account;
+  }
+}
