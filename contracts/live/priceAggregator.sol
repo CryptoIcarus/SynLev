3d0b6c2191fa245d8dd2311011aea879eea721b6
CryptoIcarus <icarus@shitcoin.fun>
2020-12-13 13:13:04 +0100

    Handle Chainlink phase change

--- contracts/live/priceAggregator.sol
+++ contracts/live/priceAggregator.sol
@@ -15,6 +15,8 @@ contract priceAggregator is Owned {
   constructor() public {
     refVault[0xFf40827Ee1c4Eb6052044101E1C6E28DBe1440e3].ref =
       AggregatorInterface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    refVault[0xA81f8460dE4008577e7e6a17708102392f9aD92D].ref =
+      AggregatorInterface(0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c);
   }
 
   struct vaultStruct{
@@ -23,39 +25,37 @@ contract priceAggregator is Owned {
 
   mapping(address => vaultStruct) public refVault;     //Vault address => vaultStruct
 
-  uint256 public maxUpdates = 50;
+  uint256 public maxUpdates = 10;
 
   function priceRequest(address vault, uint256 lastUpdated)
-  public
-  view
-  returns(int256[] memory, uint256)
-  {
-    uint256 currentRound = refVault[vault].ref.latestRound();
-    if(currentRound > lastUpdated) {
-      int256 initialPrice = refVault[vault].ref.getAnswer(lastUpdated);
-      uint256 pricearrayLength;
-      uint16 phaseId = refVault[vault].ref.phaseId();
-      if(uint16(lastUpdated >> 64) < phaseId) {
-        lastUpdated = uint256(phaseId) * 2**64 + 1;
-        pricearrayLength = currentRound.add(2).sub(lastUpdated);
+    public
+    view
+    returns(int256[] memory, uint256)
+    {
+      uint256 currentRound = refVault[vault].ref.latestRound();
+      if(currentRound > lastUpdated + 10000) {
+        int256[] memory pricearray = new int256[] (2);
+        pricearray[0] = refVault[vault].ref.getAnswer(lastUpdated);
+        pricearray[1] = refVault[vault].ref.getAnswer(currentRound);
+        return(pricearray, currentRound);
+      }
+      else if(currentRound > lastUpdated) {
+        uint256 pricearrayLength = currentRound.add(1).sub(lastUpdated);
+        pricearrayLength = pricearrayLength > maxUpdates ?
+        maxUpdates : pricearrayLength;
+        int256[] memory pricearray = new int256[] (pricearrayLength);
+        pricearray[0] = refVault[vault].ref.getAnswer(lastUpdated);
+        for(uint i = 1; i < pricearrayLength; i++) {
+          pricearray[pricearrayLength.sub(i)] =
+          refVault[vault].ref.getAnswer(currentRound.add(1).sub(i));
+        }
+        return(pricearray, currentRound);
       }
       else {
-        pricearrayLength = currentRound.add(1).sub(lastUpdated);
+        return(new int256[](0), currentRound);
       }
-      pricearrayLength = pricearrayLength > maxUpdates ?
-      maxUpdates : pricearrayLength;
-      int256[] memory pricearray = new int256[] (pricearrayLength);
-      pricearray[0] = initialPrice;
-      for(uint i = 1; i < pricearrayLength; i++) {
-        pricearray[pricearrayLength.sub(i)] =
-        refVault[vault].ref.getAnswer(currentRound.add(1).sub(i));
-      }
-      return(pricearray, currentRound);
     }
-    else {
-      return(new int256[](0), currentRound);
-    }
-  }
+
 
   /*
    * @notice Returns false if the price is not updated on vault.
